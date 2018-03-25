f703185f4b5dc743f3734a2f00eb311913c9400b
Динар Галимов <fl3nkey@MacBook-Air-Dinar.local>
2018-03-25 21:11:52 +0500

    webpack es6

--- dist/fix-scroll.js
+++ dist/fix-scroll.js
@@ -1,144 +1,232 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
 
-/*!
- * fix-scroll
- * (c) FL3N
- */
+"use strict";
 
-(function () {
-  var FixScroll;
 
-  FixScroll = function () {
-    var _bindFixScrollEvents, _scrollableClassName, _scrollableDataset, _state;
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
 
-    _state = true;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-    _scrollableClassName = 'fs--scrollable';
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    _scrollableDataset = 'fsScrollable';
+var SCROLLABLE_CLASSNAME = 'fs--scrollable';
+var PREVENT_SCROLL_DATASET = 'fsScrollable';
+var DELTA_DATASET = 'fsDelta';
 
-    _bindFixScrollEvents = function () {
-      var _findTarget;
-      _findTarget = function (e) {
-        var target;
-        target = e.target;
-        while (target !== null) {
-          if (target.classList && target.classList.contains(_scrollableClassName)) {
-            break;
-          }
-          target = target.parentNode;
-        }
-        return target;
-      };
-      document.addEventListener('touchstart', function (_this) {
-        return function (e) {
-          var height, scrollTop, target, totalScroll;
-          target = _findTarget(e);
-          if (target) {
-            scrollTop = target.scrollTop;
-            totalScroll = target.scrollHeight;
-            height = target.clientHeight;
-            target.dataset._deltaDataset = e.touches[0].clientY;
-            if (height === totalScroll) {
-              return target.dataset._preventScrollableDataset = 'true';
-            }
-          }
-        };
-      }(this));
-      document.addEventListener('touchmove', function (_this) {
-        return function (e) {
-          var currentDelta, currentScroll, delta, scrollTop, target, totalScroll;
-          if (!_this.getState()) {
-            target = _findTarget(e);
-            if (target) {
-              if (target.dataset._preventScrollableDataset === 'true') {
-                return e.preventDefault();
-              } else {
-                scrollTop = target.scrollTop;
-                totalScroll = target.scrollHeight;
-                currentScroll = scrollTop + target.offsetHeight;
-                delta = parseFloat(target.dataset._deltaDataset);
-                currentDelta = e.touches[0].clientY;
-                if (scrollTop <= 0) {
-                  if (delta < currentDelta) {
-                    return e.preventDefault();
-                  }
-                } else if (currentScroll >= totalScroll) {
-                  if (delta > currentDelta) {
-                    return e.preventDefault();
-                  }
-                }
-              }
-            }
-          }
-        };
-      }(this));
-      return document.addEventListener('touchend', function (_this) {
-        return function (e) {
-          var target;
-          target = _findTarget(e);
-          if (target) {
-            return target.dataset._preventScrollableDataset = 'false';
-          }
-        };
-      }(this));
-    };
+var findTarget = function findTarget(e) {
+	var target = e.target;
+	while (target !== null) {
+		if (target.classList && target.classList.contains(SCROLLABLE_CLASSNAME)) {
+			break;
+		};
+		target = target.parentNode;
+	};
+	return target;
+};
 
-    function FixScroll() {
-      _bindFixScrollEvents.call(this);
-    }
+var touchstartEventHandler = function touchstartEventHandler(e, fixScroll) {
+	var target = findTarget(e);
+	if (target) {
+		var scrollTop = target.scrollTop;
+		var totalScroll = target.scrollHeight;
+		var height = target.clientHeight;
+		target.dataset[DELTA_DATASET] = e.touches[0].clientY;
 
-    FixScroll.prototype.getWidth = function () {
-      var width;
-      document.body.style.overflow = 'scroll';
-      width = this.getCurrentWidth();
-      document.body.style.overflow = '';
-      return width;
-    };
+		if (height === totalScroll) {
+			target.dataset[PREVENT_SCROLL_DATASET] = 'true';
+		};
+	};
+};
 
-    FixScroll.prototype.getCurrentWidth = function () {
-      var currentWidth, documentWidth, windowWidth;
-      documentWidth = document.documentElement.clientWidth;
-      windowWidth = window.innerWidth;
-      currentWidth = windowWidth - documentWidth;
-      return currentWidth;
-    };
+var touchmoveEventHandler = function touchmoveEventHandler(e, fixScroll) {
+	if (!fixScroll.getState()) {
+		var target = findTarget(e);
+		if (target) {
+			if (target.dataset[PREVENT_SCROLL_DATASET] === 'true') {
+				e.preventDefault();
+			} else {
+				var scrollTop = target.scrollTop;
+				var totalScroll = target.scrollHeight;
+				var currentScroll = scrollTop + target.offsetHeight;
+				var delta = parseFloat(target.dataset[DELTA_DATASET]);
+				var currentDelta = e.touches[0].clientY;
 
-    FixScroll.prototype.getState = function () {
-      return _state;
-    };
+				if (scrollTop <= 0) {
+					if (delta < currentDelta) {
+						e.preventDefault();
+					};
+				} else if (currentScroll >= totalScroll) {
+					if (delta > currentDelta) {
+						e.preventDefault();
+					};
+				};
+			};
+		};
+	};
+};
 
-    FixScroll.prototype.hide = function () {
-      var currentWidth;
-      currentWidth = this.getCurrentWidth();
-      document.body.style.overflow = 'hidden';
-      document.body.style.paddingRight = currentWidth + 'px';
-      return _state = false;
-    };
+var touchendEventHandler = function touchendEventHandler(e, fixScroll) {
+	var target = findTarget(e);
+	if (target) {
+		target.dataset[PREVENT_SCROLL_DATASET] = 'false';
+	};
+};
 
-    FixScroll.prototype.show = function () {
-      document.body.style.overflow = '';
-      document.body.style.paddingRight = '';
-      return _state = true;
-    };
+var bindEvents = function bindEvents(fixScroll) {
+	document.addEventListener('touchstart', function (e) {
+		return touchstartEventHandler(e, fixScroll);
+	});
+	document.addEventListener('touchmove', function (e) {
+		return touchmoveEventHandler(e, fixScroll);
+	});
+	document.addEventListener('touchend', function (e) {
+		return touchendEventHandler(e, fixScroll);
+	});
+};
 
-    FixScroll.prototype.toggle = function () {
-      if (this.getState()) {
-        return this.hide();
-      } else {
-        return this.show();
-      }
-    };
+var FixScroll = function () {
+	function FixScroll() {
+		_classCallCheck(this, FixScroll);
 
-    return FixScroll;
-  }();
+		this.state = true;
+		bindEvents(this);
+	}
 
-  if (typeof define === 'function' && define.amd) {
-    define([], function () {
-      return new FixScroll();
-    });
-  } else if (typeof exports === 'object') {
-    module.exports = new FixScroll();
-  } else {
-    window.fixScroll = new FixScroll();
-  }
-}).call(this);
+	_createClass(FixScroll, [{
+		key: 'getState',
+		value: function getState() {
+			return this.state;
+		}
+	}, {
+		key: 'hide',
+		value: function hide() {
+			var currentWidth = this.getCurrentWidth();
+			document.body.style.overflow = 'hidden';
+			document.body.style.paddingRight = currentWidth + 'px';
+			this.state = false;
+		}
+	}, {
+		key: 'show',
+		value: function show() {
+			document.body.style.overflow = '';
+			document.body.style.paddingRight = '';
+			this.state = true;
+		}
+	}, {
+		key: 'toggle',
+		value: function toggle() {
+			if (this.getState()) {
+				this.hide();
+			} else {
+				this.show();
+			};
+		}
+	}, {
+		key: 'getWidth',
+		value: function getWidth() {
+			var overflowCurrentProperty = document.body.style.overflow;
+			var width = 0;
+			document.body.style.overflow = 'scroll';
+			width = this.getCurrentWidth();
+			document.body.style.overflow = overflowCurrentProperty;
+			return width;
+		}
+	}, {
+		key: 'getCurrentWidth',
+		value: function getCurrentWidth() {
+			var documentWidth = document.documentElement.clientWidth;
+			var windowWidth = window.innerWidth;
+			var currentWidth = windowWidth - documentWidth;
+			return currentWidth;
+		}
+	}]);
+
+	return FixScroll;
+}();
+
+;
+
+var fixScroll = exports.fixScroll = new FixScroll();
+
+/***/ })
+/******/ ]);
+});
